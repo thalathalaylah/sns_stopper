daf3715cfaad45337eee36a0eb3bf1a862a1e069
thalathalaylah <fragment.vertex@gmail.com>
2019-02-12 21:08:05 +0900

    [add] sns_stopperのpermitとforbidを取り急ぎ実装

--- sns_stopper.sh
+++ sns_stopper.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-sh dnsmasq/init.sh
+init () {
+  sh dnsmasq/init.sh
+}
+
+forbid () {
+  sh dnsmasq/forbid.sh
+  sh app/forbid.sh
+}
+
+permit () {
+  sh dnsmasq/permit.sh
+  sh app/permit.sh
+}
+
+if [ $1 = "forbid" ]; then
+  forbid
+fi
+
+if [ $1 = "permit" ]; then
+  permit
+fi
