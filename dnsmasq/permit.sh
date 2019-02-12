bcd4f3ae7e6457abb0b238ad9199ab0071674784
thalathalaylah <fragment.vertex@gmail.com>
2019-02-12 10:06:01 +0900

    [fix] target_domainsをdnsmasqに移動

--- dnsmasq/permit.sh
+++ dnsmasq/permit.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH=$(dirname $0)
 
-cat ${SCRIPTPATH}/../target_domains | while read domain
+cat ${SCRIPTPATH}/target_domains | while read domain
 do
   sudo rm "/etc/resolver/$domain"
 done
